c341a13a90af4b5f7cfab25080d5fb29dfc41008
panjihanum <pan.hanum@gmail.com>
2024-09-10 00:39:25 +0700

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,15 +2,12 @@ module.exports = {
   apps: [
     {
       name: "telegram-bot",
-      script: "nodemon",
-      args: "app.js",
-      interpreter: "node",
-      instances: 1,
+      script: "./app.js",
+      instances: 2,
       exec_mode: "fork",
       watch: true,
       ignore_watch: ["node_modules"],
       max_memory_restart: "500M",
-      node_args: "--experimental-json-modules",
       env: {
         NODE_ENV: "development",
         PORT: 4000,
